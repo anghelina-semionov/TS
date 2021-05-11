55143f0e367a20fbaa4bbdd9ccfa399bbcc6fcd9
Anghelina Semionov <anghelina.semionov@isa.utm.md>
2021-05-11 15:18:14 +0300

    Add project files.

---
+++ lab1/Complex.cs
@@ -0,0 +1,86 @@
+﻿using System;
+
+namespace lab1
+{
+    class Complex
+    {
+        private double real;
+        private double imag;
+
+        public double Real
+        {
+            get { return real; }
+            set { real = value; }
+        }
+        public double Imag
+        {
+            get { return imag; }
+            set { imag = value; }
+        }
+
+        public static string div(Complex a, Complex b)
+        {
+            //Console.WriteLine("Вершина 4");
+            //Console.WriteLine("Вершина 5");
+            double a1 = a.real;
+            double b1 = a.imag;
+            double a2 = b.real;
+            double b2 = b.imag;
+
+            Complex res = new Complex();
+
+            double den = a2 * a2 + b2 * b2;
+
+           // Console.WriteLine("Вершина 6");
+            if (Double.IsInfinity(den))
+            {
+                //Console.WriteLine("Вершина 7");
+                //Console.WriteLine("Переполнение: невозможно выполнить деление.");
+                return "Переполнение: невозможно выполнить деление.";
+                //throw new ArithmeticException();
+                // Console.WriteLine("Вершина 8");
+            }
+            //Console.WriteLine("Вершина 9");
+            if (den == 0)
+            {
+            //    Console.WriteLine("Вершина 10");
+                //Console.WriteLine("Попытка деления на нуль!");
+                return "Попытка деления на нуль!";
+                //Console.WriteLine("Вершина 11");
+            }
+
+            res.real = (a1 * a2 + b1 * b2) / den;
+            res.imag = (a2 * b1 - a1 * b2) / den;
+
+            //Console.WriteLine("Вершина 12");
+
+            //Console.WriteLine("Вершина 13");
+            if (res.real == 0 && res.imag != 0)
+            {
+                //Console.WriteLine("Вершина 14, 15");
+                //Console.WriteLine($"Результат деления: z = {res.imag}*i");
+                return "Результат деления: z = " + res.imag.ToString() + "*i" ;
+               // Console.WriteLine("Вершина 16");
+            }
+            //Console.WriteLine("Вершина 17");
+            if (res.real != 0 && res.imag == 0)
+            {
+                //Console.WriteLine("Вершина 18, 19");
+               // Console.WriteLine($"Результат деления: z = {res.real}");
+                return "Результат деления: z = " + res.real.ToString() + "*i"; ;
+                //Console.WriteLine("Вершина 20");
+            }
+            //Console.WriteLine("Вершина 21");
+            if (res.real == 0 && res.imag == 0)
+            {
+               // Console.WriteLine("Вершина 22, 23");
+               // Console.WriteLine("z = 0");
+                return "Результат деления: z = 0";
+                //Console.WriteLine("Вершина 24");
+            }
+            return "Результат деления: z = " + res.real.ToString() + " + (" + res.imag.ToString() + ")*i";
+            //Console.WriteLine($"Результат деления: z = {res.real} + ({res.imag})*i");
+            //Console.WriteLine("Вершина 25");
+        }
+    }
+}
---
+++ lab1/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+
+namespace lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Деление комплексных чисел";
+            Complex a = new Complex(), b = new Complex();
+            double a1 = 0, b1 = 0, a2 = 0, b2 = 0;
+            bool flag;
+            string strg = "";
+            while (true)
+            {
+                Console.Clear();
+                flag = true;
+               // Console.WriteLine("Вершина 1");
+                while (flag)
+                {
+                    try
+                    {
+                        Console.WriteLine("----------------------------------Деление комплексных чисел------------------------------------\n" +
+                        "Программа выполняет деление двух комплексных чисел:\n" +
+                        "z1 = a1 + b1*i (делимое) и z2 = a2 + b2*i (делитель).\n" +
+                        "Примечание:\nПрограмма обрабатывает 154 - разрядные числа.\n" +
+                        "Макс. число символов, которое можно ввести с клавиатуры, - 254.\n" +
+                        "При вводе десятичных дробей в качестве разделителя целой и дробной части используется запятая.\n" +
+                        "Входные данные:"
+                        );
+
+                        Console.Write("a1 = ");
+                        string str = Console.ReadLine();
+                        a1 = Convert.ToDouble(str);
+
+                        Console.Write("b1 = ");
+                        str = Console.ReadLine();
+                        b1 = Convert.ToDouble(str);
+
+                        Console.Write("a2 = ");
+                        str = Console.ReadLine();
+                        a2 = Convert.ToDouble(str);
+
+                        Console.Write("b2 = ");
+                        str = Console.ReadLine();
+                        b2 = Convert.ToDouble(str);
+
+                        flag = false;
+                    }
+                    catch (FormatException)
+                    {
+                        Console.WriteLine("Неверный формат данных!");
+                        System.Threading.Thread.Sleep(500);
+                        Console.Clear();
+                    }
+                }
+
+                Console.WriteLine($"Получены следующие комплексные числа:\nz1 = {a1} + ({b1})*i");
+                Console.WriteLine($"z2 = {a2} + ({b2})*i");
+
+               // Console.WriteLine("Вершина 2");
+
+                a.Real = a1; a.Imag = b1;
+                b.Real = a2; b.Imag = b2;
+
+               // Console.WriteLine("Вершина 3");
+
+                strg = Complex.div(a, b);
+                Console.WriteLine(strg);
+
+                Console.WriteLine("Нажмите <п>, чтобы продолжить, или <Ctrl + C>, чтобы закрыть программу.");
+               // Console.WriteLine("Вершина 26");
+                string ch = Console.ReadLine();
+              //  Console.WriteLine("Вершина 27");
+                if (ch != "п")
+                {
+               //     Console.WriteLine("Вершина 28");
+                    Environment.Exit(0);
+                }
+              //  Console.WriteLine("Вершина 29");
+            }
+        }
+    }
+}
